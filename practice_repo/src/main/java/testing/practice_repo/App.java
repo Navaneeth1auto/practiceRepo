59747aa325c1f56ae9479a682a49a649f3b61ec8
Navaneeth <navaneeth.info@gmail.com>
2022-01-14 08:21:04 +0530

    adding Maven Structure to Project

---
+++ practice_repo/src/main/java/testing/practice_repo/App.java
@@ -0,0 +1,13 @@
+package testing.practice_repo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
